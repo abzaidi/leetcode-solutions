b1fe82015870f27ba7b75e63927304467bf86ba5
Abubakar Zaidi <98954631+abzaidi@users.noreply.github.com>
2024-08-01 22:02:05 +0500

    Delete no_of_senior_citizens.py

--- no_of_senior_citizens.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def countSeniors(self, details: List[str]) -> int:
-        res = 0
-        for detail in details:
-            age = int(detail[11] + detail[12])
-            if age > 60:
-                res += 1
-        return res
